3312ef5ddfd2d5a1ace4efaa371de3213d968348
ly49nkallo <65131672+ly49nkallo@users.noreply.github.com>
2021-04-11 23:45:17 -0700

    r

--- othello.py
+++ othello.py
@@ -5,16 +5,73 @@ WHITE = 2
 
 def IX(i, j):
     # Use this to index into game board
-    return (i + (j * 8))
+    return (j + (i * 8))
 
 def initial_state():
+
     # the starting posistion of every new othello game
-    b = [EMPTY] * (8 * 8)
+
+    b = [EMPTY] * (8 * 8)  # we use a one dimentional array because it is theoretically faster (and I feel like it)
     b[IX(3,3)] = BLACK
     b[IX(4,4)] = BLACK
     b[IX(3,4)] = WHITE
     b[IX(4,3)] = WHITE
     return b
 
+class Othello():
+    
+    # controls all aspects of game session, including adversarial AI (not intellegent just random)
+
+    def __init__(self):
+        self.board = initial_state()
+    
+    def terminal(self):
+        # test if the current board is a terminal state
+        for tile in self.board:
+            if tile == EMPTY:
+                return False
+        return True
+    
+    def player(self):
+        # return current player to make move (assuming white goes first)
+        whiteCount = 0
+        blackCount = 0
+        for tile in self.board:
+            if tile == BLACK:
+                blackCount += 1
+            if tile == WHITE:
+                whiteCount += 1
+
+        if whiteCount == blackCount:
+            return WHITE
+        else:
+            return BLACK
+
+    def winner(self):  
+        # return a string if you would be so nice (white or black)
+        raise NotImplementedError
+    
+    def result(self, move):
+        # non destructive board + move result
+        board = self.board.copy()
+        board[IX(move[0], move[1])] = self.player()
+        return board
+
+        raise NotImplementedError
+    
+    def make_random_move(self):
+
+        raise NotImplementedError
+    
+    def make_greedy_move(self):
+
+        raise NotImplementedError
+    
+    def change_board(self, move):
+
+        self.board[IX(move[0], move[1])] = self.player()
+
+    
+    #def make_move(self):
 
 
--- runner.py
+++ runner.py
@@ -29,38 +29,50 @@ board_height = height - (BOARD_PADDING * 2)
 cell_size = int(min(board_width / 8, board_height / 8))
 board_origin = (BOARD_PADDING, BOARD_PADDING)
 
-board = o.initial_state()
+game = o.Othello()
 ai_turn = False
 
 user = None
+
+Instructions = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     
-    screen.fill(green)
+    screen.fill(black)
 
-    if user is None:
+    if Instructions:
+        continue
+
+    elif user is None:
         title = largeFont.render("Play Othello", True, white)
         titleRect = title.get_rect()
         titleRect.center = ((width / 2), 50)
         screen.blit(title, titleRect)
 
         # Draw buttons
-        playXButton = pygame.Rect((width / 8), (height / 2), width / 4, 50)
+        playXButton = pygame.Rect((width / 16), (height / 2), width / 4, 50)
         playX = mediumFont.render("Play white", True, black)
         playXRect = playX.get_rect()
         playXRect.center = playXButton.center
         pygame.draw.rect(screen, white, playXButton)
         screen.blit(playX, playXRect)
 
-        playOButton = pygame.Rect(5 * (width / 8), (height / 2), width / 4, 50)
+        playOButton = pygame.Rect(6 * (width / 16), (height / 2), width / 4, 50)
         playO = mediumFont.render("Play black", True, black)
         playORect = playO.get_rect()
         playORect.center = playOButton.center
         pygame.draw.rect(screen, white, playOButton)
         screen.blit(playO, playORect)
+
+        instructionsButton = pygame.Rect(11 * (width / 16), (height / 2), width/4, 50)
+        instructions = mediumFont.render("Instructions", True, black)
+        instructionRect = instructions.get_rect()
+        instructionRect.center = instructionsButton.center
+        pygame.draw.rect(screen, white, instructionsButton)
+        screen.blit(instructions, instructionRect)
 
         # Check if button is clicked
         click, _, _ = pygame.mouse.get_pressed()
@@ -68,29 +80,79 @@ while True:
             mouse = pygame.mouse.get_pos()
             if playXButton.collidepoint(mouse):
                 time.sleep(0.2)
-                user = o.BLACK
+                user = o.WHITE
             elif playOButton.collidepoint(mouse):
                 time.sleep(0.2)
-                user = o.WHITE
+                user = o.BLACK
+            elif instructionsButton.collidepoint(mouse):
+                time.sleep(0.2)
+                instructions = True
+
     else:
-        tile_size = 80
-        tile_origin = (width / 2 - (1.5 * tile_size),
-                       height / 2 - (1.5 * tile_size))
-        tiles = []
-        for i in range(3):
-            row = []
-            for j in range(3):
+        tile_size = 50
+        tile_origin = (width / 2 - (4 * tile_size),
+                       height / 2 - (4 * tile_size))
+        collision_tiles = []
+        for i in range(8):
+            for j in range(8):
                 rect = pygame.Rect(
                     tile_origin[0] + j * tile_size,
                     tile_origin[1] + i * tile_size,
                     tile_size, tile_size
                 )
-                pygame.draw.rect(screen, white, rect, 3)
+                pygame.draw.rect(screen, white, rect, 5)
+                pygame.draw.rect(screen, green, rect)
 
-                if board[i][j] != ttt.EMPTY:
-                    move = moveFont.render(board[i][j], True, white)
-                    moveRect = move.get_rect()
-                    moveRect.center = rect.center
-                    screen.blit(move, moveRect)
-                
+                # draw game pieces
+                radius = 20
+                board = game.board
+                center = (
+                    tile_origin[0] + j * tile_size + tile_size/2,
+                    tile_origin[1] + i * tile_size + tile_size/2,
+                )
+                if board[o.IX(i, j)] == 1:
+                    pygame.draw.circle(screen, black, center, radius)
+                if board[o.IX(i, j)] == 2:
+                    pygame.draw.circle(screen, white, center, radius)
+                collision_tiles.append(rect)
+
+        game_over = game.terminal()
+        player = game.player()
+
+        if game_over:
+            winner = game.winner()
+            if winner is None:
+                title = f"Game Over: Tie."
+            else:
+                title = f"Game Over: {winner} wins."
+        elif user == player:
+            if user == o.BLACK:
+                title = "Play as black"
+            else:
+                title = "Play as white"
+        else:
+            title = f"Computer thinking..."
+        title = largeFont.render(title, True, white)
+        titleRect = title.get_rect()
+        titleRect.center = ((width / 2), 30)
+        screen.blit(title, titleRect)
+
+        if user != player and not game_over:
+            if ai_turn:
+                time.sleep(0.5)
+                move = game.make_random_move()
+                game.change_board(move)
+                ai_turn = False
+            else:
+                ai_turn = True
+        
+        click, x, y = pygame.mouse.get_pressed()
+        if click == 1 and user == player and not game_over:
+            mouse = pygame.mouse.get_pos()
+            for i in range(8):
+                for j in range (8):
+                    if game.board[o.IX(i, j)] == o.EMPTY and collision_tiles[o.IX(i, j)].collidepoint(mouse):
+                        game.change_board((i, j))
+        
+
     pygame.display.flip()
